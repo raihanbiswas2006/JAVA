ed436eef1911241ebe9c257ae70b4db1b063e09e
raihanbiswas2006 <raihanbiswas2006@gmail.com>
2026-01-18 00:58:28 +0600

    Java First Project Open

---
+++ Raihan_1032_batch66/src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.out.println("Raihan Biswas. ID: 1032, Batch: 66th.");
+    }
+}
