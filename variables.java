ab0f91dac43fe4d747dc17076f8d541cdb0945e5
raihanbiswas2006 <raihanbiswas2006@gmail.com>
2026-03-11 23:30:00 +0600

    Variables

---
+++ variables.java
@@ -0,0 +1,16 @@
+public class variables {
+    public static void main(String[] args) {
+
+        int myNum = 5; // Integer (whole number)
+        float myFloatNum = 5.99f; // Floating point number
+        char myLetter = 'D'; // Character
+        boolean myBool = true; // Boolean
+        String myText = "Hello"; // String
+
+        System.out.println(myNum);
+        System.out.println(myFloatNum + " is a floating point number");
+        System.out.println(myLetter + " is a character");
+        System.out.println("Bool" + myBool);
+        System.out.print(myText);
+    }
+}
