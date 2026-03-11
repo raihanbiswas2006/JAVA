37e9b9e3cf6395df7f60d82ee9b15d6ef6dff52b
raihanbiswas2006 <raihanbiswas2006@gmail.com>
2026-03-11 23:45:08 +0600

    Print Star

---
+++ print_star.java
@@ -0,0 +1,8 @@
+public class print_star {
+    public static void main(String[] args) {
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+    }
+}
