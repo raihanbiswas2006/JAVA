67895c65c6ebb8dfec92a50c276fb3e80279bd0d
raihanbiswas2006 <raihanbiswas2006@gmail.com>
2026-03-12 23:59:06 +0600

    Variables Define

---
+++ variables_define.java
@@ -0,0 +1,17 @@
+public class variables_define {
+    public static void main(String[] args) {
+
+        int a = 10; // Integer (whole number)
+        int b = 20;
+        int sum = a + b; // Sum of a and b
+        System.out.println(sum);
+        int difference = a - b; // Difference of a and b
+        System.out.println(difference);
+        int mul = a * b; // Multiplication of a and b
+        System.out.println(mul);
+        int div = a / b; // Division of a and b
+        System.out.println(div);
+        int mod = a % b; // Modulus of a and b
+        System.out.println(mod);
+    }
+}
