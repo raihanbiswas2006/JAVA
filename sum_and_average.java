183938fc2a615e62d931409a00eca951cbaae4fc
raihanbiswas2006 <raihanbiswas2006@gmail.com>
2026-04-17 23:52:43 +0600

    Practice Java

---
+++ sum_and_average.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class sum_and_average {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum = 0;
+
+        int[] a = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+            sum = sum + a[i];
+        }
+
+        double avg = sum / (double) n;
+
+        System.out.println("Sum = " + sum);
+        System.out.println("Average = " + avg);
+    }
+}
