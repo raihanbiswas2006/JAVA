8c6887d45d886e434810f9bb7821c27d5466588b
raihanbiswas2006 <raihanbiswas2006@gmail.com>
2026-04-21 01:58:28 +0600

    Update Code

--- LargestSmallest.java
+++ LargestSmallest.java
@@ -3,12 +3,12 @@ import java.util.Scanner;
 public class LargestSmallest {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter size: ");
+        System.out.print("Give Size: ");
         int n = sc.nextInt();
 
         int[] arr = new int[n];
 
-        System.out.println("Enter elements:");
+        System.out.println("Give Elements:");
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
