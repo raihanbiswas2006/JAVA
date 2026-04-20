ea41184530e03e9eb2042eec8c775b40c856b050
raihanbiswas2006 <raihanbiswas2006@gmail.com>
2026-04-21 01:57:33 +0600

    Update UI

--- input_output.java
+++ input_output.java
@@ -3,10 +3,10 @@ import java.util.*;
 public class input_output {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Give your name: ");
+        System.out.print("Give your Name: ");
         String name = scanner.nextLine();
-        System.out.print("Give your age: ");
+        System.out.print("Give your Age: ");
         int age = scanner.nextInt();
-        System.out.println("Hello, " + name + "! You are " + age + " years old.");
+        System.out.println("Hello, " + name + "! You are " + age + " Years Old.");
     }
 }
