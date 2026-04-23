6407f4c9d32ef0be1bbc2f58903a18a776c078d3
raihanbiswas2006 <raihanbiswas2006@gmail.com>
2026-04-23 23:47:32 +0600

    Update MaxMin

--- maxmin.java
+++ maxmin.java
@@ -5,12 +5,12 @@ public static void main(String[] args){
     System.out.println("enter array size : ");
     Scanner sc= new Scanner(System.in);
     int n = sc.nextInt();
-    System.out.println("Enter array numbers : ");
+    System.out.println("Give array numbers : ");
     int arr[] = new int[n];
     for(int i = 0;i<n;i++){
         arr[i]= sc.nextInt();
     }
-    System.out.println("Enter array num is :");
+    System.out.println("Given array numbers are :");
     for(int i =0;i<n;i++){
         System.out.println(arr[i]);
     }
